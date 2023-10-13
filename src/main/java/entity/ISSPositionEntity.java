9a1fd185fdef6eebf85455c730b9ab828e2e09b5
Tomas Pitron <91890639+Mklfcqp@users.noreply.github.com>
2023-10-13 12:54:15 +0200

    Update ISSPositionEntity.java
    change - ISSPositionEntity

---
+++ src/main/java/entity/ISSPositionEntity.java
@@ -0,0 +1,37 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "position")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ISSPositionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "longitude")
+    private double longitude;
+
+    @Column(name = "latitude")
+    private double latitude;
+
+    @Column(name = "timestamp")
+    private long timestamp;
+
+    @Column(name = "time")
+    private String time;
+    //private LocalDate time;
+
+}

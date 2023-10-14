ad7112e88d69b1375a852080045e42445efa32ac
Martin Blazek <martinblazek6@gmail.com>
2023-10-14 15:48:55 +0200

    try

--- src/main/test/JsonWorkerTest.java
+++ src/main/test/JsonWorkerTest.java
@@ -1,30 +1,11 @@
-import com.google.gson.JsonElement;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class JsonWorkerTest {
-
-    @Mock
-    private JsonWorker jsonWorker;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        jsonWorker = new JsonWorker();
-    }
+class JsonWorkerTest {
 
     @Test
-    public void testJsonParser() throws IOException {
-        String jsonFilePath = "src/iss.json";
-        JsonElement jsonElement = jsonWorker.jsonParser(jsonFilePath);
-        assertEquals(2,3);
-        assertNotNull(jsonElement);
-        assertTrue(jsonElement.isJsonObject());
+    void jsonParser() {
+        assertTrue(1>2);
     }
 }

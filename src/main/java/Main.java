5dd4b535d8dd283d52108009c3584bd3fdd73bae
Martin Dutko <dutkomartin202@gmail.com>
2023-10-14 13:42:41 +0200

    some code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,11 @@ import json.JsonWorker;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
-
     public static void main(String[] args) throws IOException, InterruptedException {
-
         String jsonFilePathPeople = "iss.json";
         String jsonFilePathPosition = "issposition.json";
 
@@ -28,5 +28,24 @@ public class Main {
         int astronautCount = DbOperations.countCosmonauts();
 
         System.out.println("Total number of astronauts: " + astronautCount);
+
+        // Create a Scanner object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the astronaut to delete (or press Enter to delete the entire database): ");
+        String astronautName = scanner.nextLine();
+
+        if (astronautName.isEmpty()) {
+            // If the input is empty, delete the entire database
+            DbOperations.deleteDatabase();
+            System.out.println("Entire database has been deleted.");
+        } else {
+            // Otherwise, delete the astronaut by name
+            DbOperations.deletePersonByName(astronautName);
+            System.out.println("Astronaut " + astronautName + " has been deleted.");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
+
--- src/main/java/database/DbOperations.java
+++ src/main/java/database/DbOperations.java
@@ -1,6 +1,7 @@
 package database;
 
 import entity.ISSPositionEntity;
+import entity.PersonEntity;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -9,13 +10,11 @@ import java.util.List;
 
 public class DbOperations {
 
-
     public List<ISSPositionEntity> ISSPositionDataToList() {
-
         Session session = DbConnect.getSession();
         Transaction transaction = session.beginTransaction();
 
-        List<ISSPositionEntity> positionsFromDatabase = session.createQuery("From ISSPositionEntity").list();
+        List<ISSPositionEntity> positionsFromDatabase = session.createQuery("FROM ISSPositionEntity").list();
         List<ISSPositionEntity> positions = positionsFromDatabase;
 
         transaction.commit();
@@ -23,19 +22,53 @@ public class DbOperations {
 
         return positions;
     }
+
     public static int countCosmonauts() {
         Session session = DbConnect.getSession();
         try {
             session.beginTransaction();
             Query<Long> query = session.createQuery("SELECT COUNT(*) FROM PersonEntity");
-            long pocet = query.getSingleResult();
+            long count = query.getSingleResult();
             session.getTransaction().commit();
-            return (int) pocet;
+            return (int) count;
         } catch (Exception e) {
             e.printStackTrace();
             return 0;
         } finally {
             session.close();
         }
+    }
+
+    public static void deleteDatabase() {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        // Delete all records in PersonEntity
+        Query deletePeopleQuery = session.createQuery("DELETE FROM PersonEntity");
+        int deletedPeople = deletePeopleQuery.executeUpdate();
+
+        // Delete all records in ISSPositionEntity
+        Query deletePositionQuery = session.createQuery("DELETE FROM ISSPositionEntity");
+        int deletedPositions = deletePositionQuery.executeUpdate();
+
+        transaction.commit();
+        session.close();
+
+        System.out.println("Deleted " + deletedPeople + " people records.");
+        System.out.println("Deleted " + deletedPositions + " position records.");
+    }
+
+    public static void deletePersonByName(String name) {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Query deletePersonQuery = session.createQuery("DELETE FROM PersonEntity WHERE name = :name");
+        deletePersonQuery.setParameter("name", name);
+
+        int deletedCount = deletePersonQuery.executeUpdate();
+        transaction.commit();
+        session.close();
+
+        System.out.println("Deleted " + deletedCount + " person records with name " + name);
     }
 }

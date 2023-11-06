a422338126d9aae8d8fd8493a42e67e37a6888dc
Tomas Pitron <91890639+Mklfcqp@users.noreply.github.com>
2023-11-06 13:26:32 +0100

    Update CraftEntity.java

--- src/main/java/entity/CraftEntity.java
+++ src/main/java/entity/CraftEntity.java
@@ -1,15 +1,28 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "craft")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CraftEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-
-
-
-
-
-
-
+    @Column(name = "name")
+    private String name;
 
     @OneToOne
     @JoinColumn(name="craft_id")

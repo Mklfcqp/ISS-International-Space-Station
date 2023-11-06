a74b9a451f6f80ee778d8bc9ae7bf8efd21a98a3
Tomas Pitron <91890639+Mklfcqp@users.noreply.github.com>
2023-11-06 12:59:56 +0100

    Update DbOperations.java

--- src/main/java/database/DbOperations.java
+++ src/main/java/database/DbOperations.java
@@ -50,7 +50,183 @@ public class DbOperations {
             session.close();
         }
     }
+//---------------------Vyhledat vsechny stanice---------------------
 
+    private static void printAllCrafts() {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<CraftEntity> crafts = session.createQuery("FROM CraftEntity").getResultList();
+
+        for (CraftEntity craft : crafts) {
+            System.out.println("Craft: " + craft.getName());
+        }
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Vyhledat vsechny lidi---------------------
+
+    private static void printAllPeople() {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<PersonEntity> crafts = session.createQuery("FROM PersonEntity").getResultList();
+
+        for (PersonEntity person : people) {
+            System.out.println("Person: " + person.getName() + ", Craft: " +person.getCraft());
+        }
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Vyhledat vsechny lidi dle stanice---------------------
+/*
+    private static void selects2Tables(Session session){
+
+        MovieEntity movie = session.find(MovieEntity.class, 1);
+
+        System.out.println("----------------------------------");
+        System.out.println("FILM-> idfilmu=" + movie.getId() + ";  jmenofilmu=" + movie.getName() + "; directorname=" + movie.getDirector().getName());
+        System.out.println("----------------------------------");
+
+
+        DirectorEntity director = session.find(DirectorEntity.class, 2);
+        System.out.println("REJZA-> id=" + director.getId() + "; jmeno=" + director.getName());
+
+        if(director.getMovies()!=null && director.getMovies().size()>0){
+            director.getMovies().forEach(movieEntity -> {
+                System.out.println("           - film=" + movieEntity.getName() );
+            });
+        }
+    }
+*/
+//---------------------Vyhledat lidi dle jmena---------------------
+
+    private static void printPersonByName(String personName) {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        PersonEntity person = session.find(PersonEntity.class, personName);
+        System.out.println("Person: " +person.getNAme()+ ");
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Pridani nove stanice---------------------
+
+    private static CraftEntity insertNewCraft(String craftName){
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        CraftEntity newCraft = new CraftEntity();                                   // vytvoreni nove stanice
+        newCraft.setName(craftName);                                                // nastaveni jmena stanice
+        newCraft.setPosition(null);                                                 // nastaveni polohy stanice
+        session.persist(newCraft);                                                  // ulozeni do db
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Pridani noveho cloveka a k nemu stanici---------------------
+
+    private static void addPersonAndItsCraft(String personName, String craftName){
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        CraftEntity craft = session.find(CraftEntity.class, craftName);             // nacteni stanice z db
+        PersonEntity newPerson = new PersonEntity();                                // vytvoreni nove osoby
+        newPerson.setName(personName);                                              // nastaveni jmena osoby
+        newPerson.setCraft(craft);                                                  // prirazeni stanice
+        session.persist(newPerson);                                                 // ulozeni do db
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Update jmena stanice---------------------
+
+    private static void updateCraftName(String oldName, String newName){
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        CraftEntity craft = session.find(CraftEntity.class, oldName);               // nacteni z db
+        craft.setName(newName);                                                     // zmena jmena
+        session.persist(craft);                                                     // ulozeni do db
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Update jmena cloveka---------------------
+
+    private static void updatePersonName(String oldName, String newName){
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        PersonEntity person = session.find(PersonEntity.class, oldName);            // nacteni z db
+        craft.setName(newName);                                                     // zmena jmena
+        session.persist(person);                                                    // ulozeni do db
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Update zmena stanice u cloveka---------------------
+
+    private static void updatePersonCraft(String personName, String craftName) {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+        
+        PersonEntity person = session.find(PersonEntity.class, personName);         // nacteni person z db
+        CraftEntity craft = session.find(CraftEntity.class, craftName);             // nacteni stanice z db
+        person.setCraft(craft);                                                     // nastaveni stanice pro person
+        session.persist(person);                                                    // ulozeni person do db
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Smazat stanici---------------------
+
+    private static void deleteCraft(String craftName) {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        CraftEntity craft = session.find(CraftEntity.class, craftName);             // vyhledani stanice v db
+        if(craft!=null)                                                             // pokud existuje v db
+            session.remove(craft);                                                  // mazeme zaznam dle entity
+
+        transaction.commit();
+        session.close();
+    }
+
+//---------------------Smazat cloveka---------------------
+
+    private static void deletePerson(String personName) {
+        Session session = DbConnect.getSession();
+        Transaction transaction = session.beginTransaction();
+
+        PersonEntity person = session.find(PersonEntity.class, personName);          // vyhledani osoby v db
+        if(person!=null)                                                             // pokud existuje v db
+            session.remove(person);                                                  // mazeme zaznam dle entity
+
+        transaction.commit();
+        session.close();
+    }
+
+
+    
+
+
+    
+
+
+
+
+    
 
     public void ISSspeed() {
 
@@ -68,4 +244,8 @@ public class DbOperations {
 
     }
 
+    
+
+
+    
 }
